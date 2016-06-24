bf5e3cdfc7520f9c1132d9db96e8c3fe8c89ea9d
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-06-24 20:20:43 +0200

    Add the generate mix task to generate schemes

---
+++ lib/generate.ex
@@ -0,0 +1,56 @@
+defmodule Mix.Tasks.Generate do
+  def run(_) do
+    [
+      light: [
+        black:   "292C36",
+        red:     "D2242E",
+        green:   "84BD5C",
+        yellow:  "C9794B",
+        blue:    "716EB1",
+        magenta: "A52281",
+        cyan:    "379D92",
+        white:   "A4A9BA",
+
+        bright_black:   "3F4453",
+        bright_red:     "F10513",
+        bright_green:   "7FDA3F",
+        bright_yellow:  "E6712E",
+        bright_blue:    "5852CD",
+        bright_magenta: "BE098C",
+        bright_cyan:    "1DB8A7",
+        bright_white:   "FFFFFF",
+
+        background: "FFFFFF",
+        bold_text: "292C36",
+        text: "292C36"
+      ],
+      dark: [
+        black:   "292C36",
+        red:     "D2242E",
+        green:   "84BD5C",
+        yellow:  "C9794B",
+        blue:    "716EB1",
+        magenta: "A52281",
+        cyan:    "379D92",
+        white:   "A4A9BA",
+
+        bright_black:   "3F4453",
+        bright_red:     "F10513",
+        bright_green:   "7FDA3F",
+        bright_yellow:  "E6712E",
+        bright_blue:    "5852CD",
+        bright_magenta: "BE098C",
+        bright_cyan:    "1DB8A7",
+        bright_white:   "FFFFFF",
+
+        background: "292C36",
+        bold_text: "FFFFFF",
+        text: "FFFFFF"
+      ]
+    ] 
+    |> Enum.each(fn({key, colors}) ->
+      plist = colors |> Colorscheme.Terminal.to_plist
+      "wwdc16-#{key}.terminal" |> File.write!(plist)
+    end)
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,20 @@
+defmodule Wwdc16.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :wwdc16,
+     version: "0.1.0",
+     elixir: "~> 1.3-rc",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:logger]]
+  end
+
+  def deps do
+    [{:colorscheme, git: "git://github.com/jeffkreeftmeijer/colorscheme.git"}]
+  end
+end
