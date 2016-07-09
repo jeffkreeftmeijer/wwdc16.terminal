0dc1022f774cccca0af4aa638e68a342d49c145f
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-07-09 23:01:39 +0200

    Use Color in lib/generate

--- mix.exs
+++ mix.exs
@@ -15,6 +15,9 @@ defmodule Wwdc16.Mixfile do
   end
 
   def deps do
-    [{:colorscheme, git: "git://github.com/jeffkreeftmeijer/colorscheme.git"}]
+    [
+      {:color, git: "git://github.com/jeffkreeftmeijer/color.git"},
+      {:colorscheme, git: "git://github.com/jeffkreeftmeijer/colorscheme.git"}
+    ]
   end
 end
