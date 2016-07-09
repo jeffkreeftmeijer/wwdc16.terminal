69463d3d54c386e5eca5a64aa7590eccf10a8cd8
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-07-09 23:10:02 +0200

    Define colors once in lib/generate

--- lib/generate.ex
+++ lib/generate.ex
@@ -1,52 +1,36 @@
 defmodule Mix.Tasks.Generate do
   def run(_) do
+    colors = [
+      black:   "292C36" |> Color.from_hexadecimal,
+      red:     "D2242E" |> Color.from_hexadecimal,
+      green:   "84BD5C" |> Color.from_hexadecimal,
+      yellow:  "C9794B" |> Color.from_hexadecimal,
+      blue:    "716EB1" |> Color.from_hexadecimal,
+      magenta: "A52281" |> Color.from_hexadecimal,
+      cyan:    "379D92" |> Color.from_hexadecimal,
+      white:   "A4A9BA" |> Color.from_hexadecimal,
+
+      bright_black:   "3F4453" |> Color.from_hexadecimal,
+      bright_red:     "F10513" |> Color.from_hexadecimal,
+      bright_green:   "7FDA3F" |> Color.from_hexadecimal,
+      bright_yellow:  "E6712E" |> Color.from_hexadecimal,
+      bright_blue:    "5852CD" |> Color.from_hexadecimal,
+      bright_magenta: "BE098C" |> Color.from_hexadecimal,
+      bright_cyan:    "1DB8A7" |> Color.from_hexadecimal,
+      bright_white:   "FFFFFF" |> Color.from_hexadecimal,
+    ]
+
     [
-      light: [
-        black:   "292C36" |> Color.from_hexadecimal,
-        red:     "D2242E" |> Color.from_hexadecimal,
-        green:   "84BD5C" |> Color.from_hexadecimal,
-        yellow:  "C9794B" |> Color.from_hexadecimal,
-        blue:    "716EB1" |> Color.from_hexadecimal,
-        magenta: "A52281" |> Color.from_hexadecimal,
-        cyan:    "379D92" |> Color.from_hexadecimal,
-        white:   "A4A9BA" |> Color.from_hexadecimal,
-
-        bright_black:   "3F4453" |> Color.from_hexadecimal,
-        bright_red:     "F10513" |> Color.from_hexadecimal,
-        bright_green:   "7FDA3F" |> Color.from_hexadecimal,
-        bright_yellow:  "E6712E" |> Color.from_hexadecimal,
-        bright_blue:    "5852CD" |> Color.from_hexadecimal,
-        bright_magenta: "BE098C" |> Color.from_hexadecimal,
-        bright_cyan:    "1DB8A7" |> Color.from_hexadecimal,
-        bright_white:   "FFFFFF" |> Color.from_hexadecimal,
-
+      light: Keyword.merge(colors, [
         background: "FFFFFF" |> Color.from_hexadecimal,
         bold_text: "292C36" |> Color.from_hexadecimal,
         text: "292C36" |> Color.from_hexadecimal,
-      ],
-      dark: [
-        black:   "292C36" |> Color.from_hexadecimal,
-        red:     "D2242E" |> Color.from_hexadecimal,
-        green:   "84BD5C" |> Color.from_hexadecimal,
-        yellow:  "C9794B" |> Color.from_hexadecimal,
-        blue:    "716EB1" |> Color.from_hexadecimal,
-        magenta: "A52281" |> Color.from_hexadecimal,
-        cyan:    "379D92" |> Color.from_hexadecimal,
-        white:   "A4A9BA" |> Color.from_hexadecimal,
-
-        bright_black:   "3F4453" |> Color.from_hexadecimal,
-        bright_red:     "F10513" |> Color.from_hexadecimal,
-        bright_green:   "7FDA3F" |> Color.from_hexadecimal,
-        bright_yellow:  "E6712E" |> Color.from_hexadecimal,
-        bright_blue:    "5852CD" |> Color.from_hexadecimal,
-        bright_magenta: "BE098C" |> Color.from_hexadecimal,
-        bright_cyan:    "1DB8A7" |> Color.from_hexadecimal,
-        bright_white:   "FFFFFF" |> Color.from_hexadecimal,
-
+      ]),
+      dark: Keyword.merge(colors, [
         background: "292C36" |> Color.from_hexadecimal,
         bold_text: "FFFFFF" |> Color.from_hexadecimal,
         text: "FFFFFF" |> Color.from_hexadecimal,
-      ]
+      ])
     ] 
     |> Enum.each(fn({key, colors}) ->
       plist = colors |> Colorscheme.Terminal.to_plist
