577bb88d1a25f526b5a07a33cf3766db35588974
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-07-09 23:20:11 +0200

    Map over colors to call Color.from_hexadecimal

--- lib/generate.ex
+++ lib/generate.ex
@@ -1,24 +1,27 @@
 defmodule Mix.Tasks.Generate do
   def run(_) do
     colors = [
-      black:   "292C36" |> Color.from_hexadecimal,
-      red:     "D2242E" |> Color.from_hexadecimal,
-      green:   "84BD5C" |> Color.from_hexadecimal,
-      yellow:  "C9794B" |> Color.from_hexadecimal,
-      blue:    "716EB1" |> Color.from_hexadecimal,
-      magenta: "A52281" |> Color.from_hexadecimal,
-      cyan:    "379D92" |> Color.from_hexadecimal,
-      white:   "A4A9BA" |> Color.from_hexadecimal,
+      black:   "292C36",
+      red:     "D2242E",
+      green:   "84BD5C",
+      yellow:  "C9794B",
+      blue:    "716EB1",
+      magenta: "A52281",
+      cyan:    "379D92",
+      white:   "A4A9BA",
 
-      bright_black:   "3F4453" |> Color.from_hexadecimal,
-      bright_red:     "F10513" |> Color.from_hexadecimal,
-      bright_green:   "7FDA3F" |> Color.from_hexadecimal,
-      bright_yellow:  "E6712E" |> Color.from_hexadecimal,
-      bright_blue:    "5852CD" |> Color.from_hexadecimal,
-      bright_magenta: "BE098C" |> Color.from_hexadecimal,
-      bright_cyan:    "1DB8A7" |> Color.from_hexadecimal,
-      bright_white:   "FFFFFF" |> Color.from_hexadecimal,
+      bright_black:   "3F4453",
+      bright_red:     "F10513",
+      bright_green:   "7FDA3F",
+      bright_yellow:  "E6712E",
+      bright_blue:    "5852CD",
+      bright_magenta: "BE098C",
+      bright_cyan:    "1DB8A7",
+      bright_white:   "FFFFFF",
     ]
+    |> Enum.map(fn({key, color}) ->
+      {key, color |> Color.from_hexadecimal}
+    end)
 
     [
       light: Keyword.merge(colors, [
